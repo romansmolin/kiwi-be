fc8a83d218adfcc639abc1a936f15a162d3f67e9
Roman Smolins <romasmolin29@gmail.com>
2024-07-01 22:09:03 +0300

    Add json files for localized characterrs

--- src/domains/event-characters/routes.js
+++ src/domains/event-characters/routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const characters = require('./model')
+
+const characters_ru = require('./characters/characters_ru.json')
+const characters_lv = require('./characters/characters_lv.json')
+const characters_en = require('./characters/characters_en.json')
+
+const intl_characters = {
+    lv: characters_lv,
+    ru: characters_ru,
+    en: characters_en
+}
+
 
 router.get("/all-characters", async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 5;
+        const language = req.query.lang
+        const characters = intl_characters[language]?.characters
+
         const total = characters.length;
         const totalPages = Math.ceil(characters.length / limit);
         const startIndex = (page - 1) * limit;
